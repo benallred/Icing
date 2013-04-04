06da067c193d58c7953d8cceeccee2bafbf1532f
Ben Allred <ben@bmallred.com>
2013-04-04 15:26:57 -0600

    Add IEnumerable<T> extension Union that takes a lambda expression

---
+++ Icing/Icing.Core/Linq/Enumerable.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Icing.Linq
+{
+	/// <summary>Provides a set of static (Shared in Visual Basic) methods for querying objects that implement <see cref="IEnumerable&lt;T&gt;"/>.</summary>
+	public static class Enumerable
+	{
+		#region Methods
+
+		/// <summary>
+		/// Produces the set union of two sequences by using a predicate to compare.
+		/// </summary>
+		/// <typeparam name="TSource">The type of the elements of the input sequences.</typeparam>
+		/// <param name="first">An <see cref="IEnumerable&lt;T&gt;"/> whose distinct elements form the first set for the union.</param>
+		/// <param name="second">An <see cref="IEnumerable&lt;T&gt;"/> whose distinct elements form the second set for the union.</param>
+		/// <param name="comparer">The comparer.</param>
+		/// <returns>An <see cref="IEnumerable&lt;T&gt;"/> that contains the elements from both input sequences, excluding duplicates.</returns>
+		/// <exception cref="ArgumentNullException">first or second is null</exception>
+		public static IEnumerable<TSource> Union<TSource>(this IEnumerable<TSource> first, IEnumerable<TSource> second, Func<TSource, TSource, bool> comparer)
+		{
+			return first.Union(second, new LambdaEqualityComparer<TSource>(comparer));
+		}
+
+		#endregion
+
+		#region Helper Classes
+
+		/// <summary>Defines methods to support the comparison of objects for equality.</summary>
+		/// <typeparam name="T">
+		///	<para>The type of objects to compare.</para>
+		///	<para>This type parameter is contravariant. That is, you can use either the type you specified or any type
+		///		that is less derived. For more information about covariance and contravariance, see
+		///		<a href="http://msdn.microsoft.com/en-us/library/dd799517.aspx">Covariance and Contravariance in Generics</a>.</para>
+		/// </typeparam>
+		private class LambdaEqualityComparer<T> : IEqualityComparer<T>
+		{
+			/// <summary>Gets or sets the equality comparer.</summary>
+			/// <value>The equality comparer.</value>
+			private Func<T, T, bool> EqualityComparer { get; set; }
+
+			/// <summary>
+			/// Initializes a new instance of the <see cref="LambdaEqualityComparer{T}"/> class.
+			/// </summary>
+			/// <param name="equalityComparer">The equality comparer.</param>
+			public LambdaEqualityComparer(Func<T, T, bool> equalityComparer)
+			{
+				EqualityComparer = equalityComparer;
+			}
+
+			/// <summary>
+			/// Determines whether the specified objects are equal.
+			/// </summary>
+			/// <returns><c>true</c> if the specified objects are equal; otherwise, <c>false</c>.</returns>
+			/// <param name="x">The first object of type T to compare.</param>
+			/// <param name="y">The second object of type T to compare.</param>
+			public bool Equals(T x, T y)
+			{
+				return EqualityComparer(x, y);
+			}
+
+			/// <summary>
+			/// Returns a hash code for the specified object.
+			/// </summary>
+			/// <returns>A hash code for the specified object.</returns>
+			/// <param name="obj">The <see cref="T:System.Object"/> for which a hash code is to be returned.</param>
+			/// <exception cref="T:System.ArgumentNullException">The type of <paramref name="obj"/> is a reference type and <paramref name="obj"/> is null.</exception>
+			public int GetHashCode(T obj)
+			{
+				// If the hash codes are different, then Equals never gets called. Make sure Equals is always called by making sure the hash codes are always the same.
+				// (Underneath, the .NET code is using a set and the not (!) of a Find method to determine if the set doesn't already contain the item and should be added.
+				// Find is not bothering to call Equals unless it finds a hash code that matches.)
+//				return obj.GetHashCode();
+				return 0;
+			}
+		}
+
+		#endregion
+	}
+}
---
+++ Icing/Icing.Tests/Core/Linq/TestOf_Enumerable.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Icing.Linq;
+using Icing.TestTools.MSTest;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Icing.Tests.Core.Linq
+{
+	[TestClass]
+	public class TestOf_Enumerable
+	{
+		[TestMethod]
+		public void Union()
+		{
+			CollectionAssert.AreEquivalent(new int[] { 1, 2, 3    }, new int[] { 1, 2       }.Union(new int[] {       3    }, (i, j) => i == j).ToList());
+			CollectionAssert.AreEquivalent(new int[] { 1, 2, 3    }, new int[] { 1, 2       }.Union(new int[] {    2, 3    }, (i, j) => i == j).ToList());
+			CollectionAssert.AreEquivalent(new int[] { 1, 2, 3    }, new int[] { 1, 2       }.Union(new int[] { 1,    3    }, (i, j) => i == j).ToList());
+			CollectionAssert.AreEquivalent(new int[] { 1, 2, 3    }, new int[] { 1, 2, 3    }.Union(new int[] { 1, 2, 3    }, (i, j) => i == j).ToList());
+			CollectionAssert.AreEquivalent(new int[] { 1, 2, 3, 4 }, new int[] { 1,    3, 4 }.Union(new int[] { 1, 2,    4 }, (i, j) => i == j).ToList());
+
+			CollectionAssert.AreEquivalent(new int[] {   }, new int[] {      }.Union(new int[] {   }, (i, j) => i == j).ToList());
+			CollectionAssert.AreEquivalent(new int[] { 1 }, new int[] { 1    }.Union(new int[] {   }, (i, j) => i == j).ToList());
+			CollectionAssert.AreEquivalent(new int[] { 1 }, new int[] {      }.Union(new int[] { 1 }, (i, j) => i == j).ToList());
+
+			CollectionAssert.AreEquivalent(new int[] { 1 }, new int[] { 1, 1 }.Union(new int[] {      }, (i, j) => i == j).ToList());
+			CollectionAssert.AreEquivalent(new int[] { 1 }, new int[] { 1, 1 }.Union(new int[] { 1    }, (i, j) => i == j).ToList());
+			CollectionAssert.AreEquivalent(new int[] { 1 }, new int[] {      }.Union(new int[] { 1, 1 }, (i, j) => i == j).ToList());
+			CollectionAssert.AreEquivalent(new int[] { 1 }, new int[] { 1    }.Union(new int[] { 1, 1 }, (i, j) => i == j).ToList());
+
+			ExceptionAssertEx.Throws<ArgumentNullException>(() => ((IEnumerable<int>)null).Union(new int[] { }, (i, j) => i == j));
+			ExceptionAssertEx.Throws<ArgumentNullException>(() => new int[] { }           .Union(null         , (i, j) => i == j));
+
+			CollectionAssert.AreEquivalent(new int[] { 1,    3    }, new int[] { 1, 2 }.Union(new int[] { 3, 4 }, (i, j) => i + 1 == j    ).ToList());
+			CollectionAssert.AreEquivalent(new int[] { 1,    3    }, new int[] { 3, 4 }.Union(new int[] { 1, 2 }, (i, j) => i + 1 == j    ).ToList());
+			CollectionAssert.AreEquivalent(new int[] { 1, 2,    4 }, new int[] { 2, 1 }.Union(new int[] { 4, 3 }, (i, j) => i + 1 == j    ).ToList());
+			CollectionAssert.AreEquivalent(new int[] { 1, 2, 3, 4 }, new int[] { 4, 3 }.Union(new int[] { 2, 1 }, (i, j) => i + 1 == j    ).ToList());
+
+			CollectionAssert.AreEquivalent(new int[] { 1, 2, 3, 4 }, new int[] { 1, 2 }.Union(new int[] { 3, 4 }, (i, j) => i     == j + 1 ).ToList());
+			CollectionAssert.AreEquivalent(new int[] { 1,    3, 4 }, new int[] { 3, 4 }.Union(new int[] { 1, 2 }, (i, j) => i     == j + 1 ).ToList());
+			CollectionAssert.AreEquivalent(new int[] {    2,    4 }, new int[] { 2, 1 }.Union(new int[] { 4, 3 }, (i, j) => i     == j + 1 ).ToList());
+			CollectionAssert.AreEquivalent(new int[] {    2,    4 }, new int[] { 4, 3 }.Union(new int[] { 2, 1 }, (i, j) => i     == j + 1 ).ToList());
+		}
+	}
+}
