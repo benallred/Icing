47f7a9f71ddfc62163088b4065aa34074ffd62c4
Ben Allred <ben@bmallred.com>
2013-09-24 12:37:50 -0600

    Add EnumExtensions with GetDescription method

---
+++ Icing/Icing.Core/EnumExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Icing
+{
+	/// <summary><see cref="Enum"/> extensions.</summary>
+	public static class EnumExtensions
+	{
+		/// <summary>
+		/// Gets the description for the given enumeration value.
+		/// </summary>
+		/// <param name="value">The enumeration value.</param>
+		/// <returns>The description for the given enumeration value.</returns>
+		public static string GetDescription(this Enum value)
+		{
+			DescriptionAttribute[] descriptionAttributes = (DescriptionAttribute[])value.GetType().GetField(value.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
+			return (descriptionAttributes != null && descriptionAttributes.Length > 0 ? descriptionAttributes[0].Description : null);
+		}
+	}
+}
---
+++ Icing/Icing.Tests/Core/TestOf_EnumExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Icing.Tests.Core
+{
+	[TestClass]
+	public class TestOf_EnumExtensions
+	{
+		[TestMethod]
+		public void GetDescription()
+		{
+			Assert.AreEqual(null, TestEnum.TestValue1.GetDescription());
+			Assert.AreEqual("", TestEnum.TestValue2.GetDescription());
+			Assert.AreEqual("Test description 3", TestEnum.TestValue3.GetDescription());
+		}
+
+		public enum TestEnum : short
+		{
+			TestValue1,
+			[System.ComponentModel.Description]
+			TestValue2,
+			[System.ComponentModel.Description("Test description 3")]
+			TestValue3
+		}
+	}
+}
