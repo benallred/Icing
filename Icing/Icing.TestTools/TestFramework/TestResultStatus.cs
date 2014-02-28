73b980b808540a93da69642b38cf2b9f800fadd0
benallred <ben@bmallred.com>
2014-02-28 14:28:47 -0700

    Add missing file and edit .gitignore to not catch too much

---
+++ Icing/Icing.TestTools/TestFramework/TestResultStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace Icing.TestTools.TestFramework
+{
+	/// <summary>The status of a test run.</summary>
+	public enum TestResultStatus
+	{
+		/// <summary>Passed</summary>
+		Passed = 1,
+		/// <summary>Failed</summary>
+		Failed = 2,
+		/// <summary>Inconclusive</summary>
+		Inconclusive = 3
+	}
+}
